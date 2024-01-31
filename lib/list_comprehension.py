0fe3078327b8d7313696130482653fe29d0d354c
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-01-30 22:16:14 -0500

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [(item) for item in num_list if item%2 == 0]
+    return new_list
+
+    
 
 def make_exclamation(sentence_list):
+    new_sentece_list=[f"{sentence}!" for sentence in sentence_list ]
+    return new_sentece_list
     pass
